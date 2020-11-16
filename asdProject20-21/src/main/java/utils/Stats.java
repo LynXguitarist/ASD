d986a232f171e722e44c5a0ce5353a759bc10e0d
frederico_lopes <fmc.lopes@campus.fct.unl.pt>
2020-11-16 20:43:47 +0000

    stats with avg time good

--- asdProject20-21/src/main/java/utils/Stats.java
+++ asdProject20-21/src/main/java/utils/Stats.java
@@ -2,6 +2,7 @@ package utils;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.UUID;
 
 public class Stats {
@@ -12,8 +13,6 @@ public class Stats {
 	private static Map<UUID, Long> msgSent = new HashMap<>();
 
 	private static long numberSent, numberReceived, numberBytesIn, numberBytesOut;
-	private static long sum = 0; 
-
 
 	public static Map<UUID, Long> getMsgCreated() {
 		return msgCreated;
@@ -24,12 +23,12 @@ public class Stats {
 	 * 
 	 * @param UUID
 	 */
-	public static  void addMsgCreated(UUID id) {
+	public static void addMsgCreated(UUID id) {
 		long currentTime = System.currentTimeMillis();
 		msgCreated.putIfAbsent(id, currentTime);
 	}
 
-	public static  Map<UUID, Long> getMsgSent() {
+	public static Map<UUID, Long> getMsgSent() {
 		return msgSent;
 	}
 
@@ -38,15 +37,15 @@ public class Stats {
 	 * 
 	 * @param UUID
 	 */
-	public static  void addOrUpdateMsgSent(UUID id) {
+	public static void addOrUpdateMsgSent(UUID id) {
 		msgSent.put(id, System.currentTimeMillis());
 	}
 
-	public static  void joinMsgCreated(Map<UUID, Long> map) {
+	public static void joinMsgCreated(Map<UUID, Long> map) {
 		msgCreated.putAll(map);
 	}
 
-	public static  void joinMsgSent(Map<UUID, Long> map) {
+	public static void joinMsgSent(Map<UUID, Long> map) {
 		map.forEach((id, time) -> {
 			if (msgSent.containsKey(id)) {
 				if (msgSent.get(id) < time)
@@ -55,48 +54,41 @@ public class Stats {
 				msgSent.put(id, time);
 		});
 	}
-	
-	public static long averageBroadcastLatency() { 
-		msgCreated.forEach((id, time) -> { 
-			sum += getDiffTime(id); 
-		}); 
-		return sum / msgCreated.size(); 
-	} 
 
-	public static  long getNumberSent() {
+	public static long getNumberSent() {
 		return numberSent;
 	}
 
-	public static  void setNumberSent(long num) {
+	public static void setNumberSent(long num) {
 		numberSent = num;
 	}
 
-	public static  long getNumberReceived() {
+	public static long getNumberReceived() {
 		return numberReceived;
 	}
 
-	public static  void setNumberReceived(long num) {
+	public static void setNumberReceived(long num) {
 		numberReceived = num;
 	}
 
-	public static  long getNumberBytesIn() {
+	public static long getNumberBytesIn() {
 		return numberBytesIn;
 	}
 
-	public static  void setNumberBytesIn(long num) {
+	public static void setNumberBytesIn(long num) {
 		numberBytesIn = num;
 	}
 
-	public static  long getNumberBytesOut() {
+	public static long getNumberBytesOut() {
 		return numberBytesOut;
 	}
 
 	public static void setNumberBytesOut(long num) {
 		numberBytesOut = num;
 	}
-	
+
 	public static String print() {
-		String nIn = "Number Msg In: " + numberReceived;
+		String nIn = "\nNumber Msg In: " + numberReceived;
 		String bytesIn = "\nNumber Bytes In: " + numberBytesIn;
 		String nOut = "\nNumber Msg Out: " + numberSent;
 		String bytesOut = "\nNumber Msg Out: " + numberBytesOut;
@@ -104,16 +96,24 @@ public class Stats {
 		String toString = nIn + bytesIn + nOut + bytesOut + latency;
 		return toString;
 	}
-	
-	/** 
-	 * Gets the diff between the last time a node sent the msg with the time of 
-	 * creation of the msg 
-	 *  
-	 * @param UUID 
-	 * @return time 
-	 */ 
-	private static long getDiffTime(UUID id) { 
-		return msgSent.get(id) - msgCreated.get(id); 
-	} 
+
+	private static long averageBroadcastLatency() {
+		long sum = 0;
+		for (Entry<UUID, Long> entry : msgCreated.entrySet()) {
+			sum += getDiffTime(entry.getKey());
+		}
+		return sum / msgCreated.size();
+	}
+
+	/**
+	 * Gets the diff between the last time a node sent the msg with the time of
+	 * creation of the msg
+	 * 
+	 * @param UUID
+	 * @return time
+	 */
+	private static long getDiffTime(UUID id) {
+		return msgSent.get(id) - msgCreated.get(id);
+	}
 
 }
